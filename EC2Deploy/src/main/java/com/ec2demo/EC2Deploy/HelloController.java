978840f55acc30e50eaf19cc11945819dce4990b
PravinBabar <pravinbabar23@gmail.com>
2025-09-23 23:51:51 +0530

    Initial commit

---
+++ EC2Deploy/src/main/java/com/ec2demo/EC2Deploy/HelloController.java
@@ -0,0 +1,20 @@
+package com.ec2demo.EC2Deploy;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello AWS from Spring Boot!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "App is running fine!";
+    }
+}
---
+++ EC2Deploy/src/test/java/com/ec2demo/EC2Deploy/Ec2DeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec2demo.EC2Deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2DeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
